a3155316c44be1b448297b9d774390dc2a129127
kilted-andres <andres@trusted-entity.io>
2022-12-02 15:50:33 +0100

    try to come back

--- attester/ctypeSchema.ts
+++ attester/ctypeSchema.ts
@@ -1,1 +1,24 @@
 //# create a local CType definition
+
+import * as Kilt from '@kiltprotocol/sdk-js'
+
+// Return CType with the properties matching a given schema.
+export function getCtypeSchema(): Kilt.ICType {
+  return Kilt.CType.fromProperties('ClaimTutorial', {
+    phrase: {
+      type: 'string'
+    },
+    validationPeriod: {
+      type: 'integer'
+    },
+    price: {
+        type: 'integer'
+      },
+    lie: {
+    type: 'boolean'
+    },
+    rate: {
+        type: 'number'
+      },
+  })
+}
--- attester/generateKeypairs.ts
+++ attester/generateKeypairs.ts
@@ -1,1 +1,27 @@
 // # setup the keys for the attester's DID
+
+import { mnemonicGenerate, mnemonicToMiniSecret } from '@polkadot/util-crypto'
+
+import * as Kilt from '@kiltprotocol/sdk-js'
+
+export function generateKeypairs(mnemonic = mnemonicGenerate()) {
+  const authentication = Kilt.Utils.Crypto.makeKeypairFromSeed(
+    mnemonicToMiniSecret(mnemonic)
+  )
+  const encryption = Kilt.Utils.Crypto.makeEncryptionKeypairFromSeed(
+    mnemonicToMiniSecret(mnemonic)
+  )
+  const attestation = authentication.derive(
+    '//attestation'
+  ) as Kilt.KiltKeyringPair
+  const delegation = authentication.derive(
+    '//delegation'
+  ) as Kilt.KiltKeyringPair
+
+  return {
+    authentication,
+    encryption,
+    attestation,
+    delegation
+  }
+}
